80aa253f4969282533adb296f05de88fc4cfc9b5
Jared Kozar <jared.rkozar@gmail.com>
2023-07-03 23:43:54 -0400

    Initial Commit

---
+++ MealsDB-App/MealsDB_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsDB_AppApp.swift
+//  MealsDB-App
+//
+//  Created by Jared Kozar on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MealsDB_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
