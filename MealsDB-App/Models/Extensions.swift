89909a6e4d01e3938f2e70118f65ff93088373c4
Jared Kozar <jared.rkozar@gmail.com>
2023-07-07 18:34:07 -0400

    added some more comments

--- MealsDB-App/Models/Extensions.swift
+++ MealsDB-App/Models/Extensions.swift
@@ -13,16 +13,19 @@ struct AsyncImageView: View {
     var dimension: CGFloat?
     
     init(url: String?, height: CGFloat) {
+        //user inputs the url they want the image from and the height/width (same since its a square
         self.urlString = url
         self.dimension = height
     }
 
     var body: some View {
+        //fetches the image asynchrounsly
         AsyncImage(url: URL(string: urlString ?? "")) { image in
             image.resizable()
                 .frame(width: dimension, height: dimension, alignment: .leading)
                 .cornerRadius(8.0)
         } placeholder: {
+            //displays progress view if the image is loading
             ProgressView().progressViewStyle(.circular)
         }
     }
